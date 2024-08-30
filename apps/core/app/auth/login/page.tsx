0a0972a684163299ef698cc954cbb0ee886ba43d
mrbadri <mrbadri.dev@gmail.com>
2024-08-31 02:01:18 +0330

    feat: add ui login and dashbaord page and add theme in global.css and change connfig tailwind

---
+++ apps/core/app/auth/login/page.tsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+
+import { Button } from "@repo/ui/components/button";
+import { Input } from "@repo/ui/components/input";
+import { Label } from "@repo/ui/components/label";
+import GridPattern from "@repo/ui/components/grid-pattern";
+
+const LoginPage = () => {
+  return (
+    <div className="w-full h-full  lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      {/* Background Pattern */}
+      <div className="hidden bg-muted lg:block relative shadow-[inset_-14px_1px_38px_-15px_#1d1d1d]">
+        <GridPattern />
+      </div>
+
+      {/* Login Form */}
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link
+                  href="/forgot-password"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+            <Button variant="outline" className="w-full">
+              Login with Google
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="#" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
